ff51c92e22474a3b9e1b4e25cc0c722f843b4eab
Amal Alex <amal.panackan@gmail.com>
2023-09-08 00:38:53 +0100

    Dev note added for code.py file

--- qr/code.py
+++ qr/code.py
@@ -1,14 +1,21 @@
+# code.py
+# Author: Amal Alex
+# Date: September 7, 2023
+# Description: This code.py demonstrates the Encryption and Decryption functions.
+
 from Crypto.Cipher import Blowfish
 from Crypto import Random
 import base64
 
 import constant
 
+# padding function 
 def Do_Padding(s, block_size):
     padding = constant.PADDING
     p = lambda s: s+ (block_size - len(s) % block_size) * padding
     return p(s)
 
+# Encrypt function
 def Encrypt(msg, key, block_size=8):
     key = str.encode(key)
     iv = Random.new().read(Blowfish.block_size)
@@ -17,6 +24,7 @@ def Encrypt(msg, key, block_size=8):
     Encrypted_message = iv + c.encrypt(msg.encode('ascii'))
     return base64.b64encode(Encrypted_message).decode()
 
+# Decrypt function
 def Decrypt(Encrypted_message, key):
     key = str.encode(key)
     Encrypted_message = str.encode(Encrypted_message)
